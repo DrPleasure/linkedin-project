f2a0fae8a933dab45f78de104c89d9925f91ac13
Nic <nic.dan.costea@gmail.com>
2023-01-23 17:23:05 +0100

    unique username

--- back/src/api/users/index.js
+++ back/src/api/users/index.js
@@ -3,27 +3,30 @@ import createHttpError from "http-errors";
 import q2m from "query-to-mongo";
 import { checksUserSchema, checkFilteredUserSchema, triggerBadRequest } from "./validator.js";
 import Users from "./model.js";
-
+import uniqid from "uniqid";
 const { NotFound } = createHttpError;
-
 const router = express.Router();
-
 // POST
 router.post(`/`, checksUserSchema, async (req, res, next) => {
   try {
-    const newUser = new Users(req.body);
+    let { surname } = req.body;
+    const uniqueString = `${surname + uniqid()}`;
+    console.log(uniqueString);
 
+    // come back to it - make a proper function
+    const body = { ...req.body, username: uniqueString.slice(0, uniqueString.length - 12) };
+
+    const newUser = new Users(body);
     const user = await newUser.save();
-    // console.log("POST - new user: ", newUser);
+
     res.status(201).send({
       message: `The new user has been successfully created; you can see it below`,
-      newUser: newUser,
+      newUser: user,
     });
   } catch (error) {
     next(error);
   }
 });
-
 // GET
 router.get("/", async (req, res, next) => {
   try {
@@ -37,9 +40,7 @@ router.get("/", async (req, res, next) => {
 router.get("/pagination", async (req, res, next) => {
   try {
     const mongoQuery = q2m(req.query);
-
     const total = await Users.countDocuments(mongoQuery.criteria);
-
     const users = await Users.find(mongoQuery.criteria, mongoQuery.options.fields)
       .limit(mongoQuery.options.limit) // No matter the order of usage of these 3 options, Mongo will ALWAYS go with SORT, then SKIP, then LIMIT
       .skip(mongoQuery.options.skip)
@@ -54,14 +55,11 @@ router.get("/pagination", async (req, res, next) => {
     next(error);
   }
 });
-
 // FILTER by user.name
 router.get("/filter", checkFilteredUserSchema, triggerBadRequest, async (req, res, next) => {
   try {
     const { name, email } = req.query;
-
     const filteredUsers = await Users.find({ name, email }, { title: 1, bio: 1 });
-
     if (filteredUsers.length > 0) {
       res.send(filteredUsers);
     } else {
@@ -71,7 +69,6 @@ router.get("/filter", checkFilteredUserSchema, triggerBadRequest, async (req, re
     next(error);
   }
 });
-
 // GET - single user
 router.get("/:userId", async (req, res, next) => {
   try {
@@ -86,7 +83,6 @@ router.get("/:userId", async (req, res, next) => {
     next(error);
   }
 });
-
 // PUT
 router.put("/:userId", async (req, res, next) => {
   try {
@@ -101,7 +97,6 @@ router.put("/:userId", async (req, res, next) => {
     next(error);
   }
 });
-
 // DELETE
 router.delete("/:userId", async (req, res, next) => {
   try {
@@ -116,5 +111,4 @@ router.delete("/:userId", async (req, res, next) => {
     next(error);
   }
 });
-
 export default router;
