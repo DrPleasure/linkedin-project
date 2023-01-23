6ae1f3e4832f08688fafc9bade1fa387a435db83
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2023-01-23 11:50:31 +0100

    updates

--- back/src/api/experiences/index.js
+++ back/src/api/experiences/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const usersModel = require('../users/model.js'); // import database here
+const User = require('../users/model.js'); // import database here
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const json2csv = require('json2csv').parse;
+const mongoose = require('mongoose');
+
 
 
 const storage = multer.diskStorage({
@@ -19,148 +21,167 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Get user experiences
-router.get('/api/users/:userId/experiences', (req, res) => {
-  db.Experience.findAll({
-    where: {
-      userId: req.params.userId
-    }
-  })
-    .then((experiences) => {
-      res.json(experiences);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+experiencesRouter.get('/api/users/:userId/experiences', (req, res) => {
+    User.findById(req.params.userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user.experiences);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  });
+  
+  // Create an experience
+  experiencesRouter.post('/api/users/:userId/experiences', (req, res) => {
+    User.findById(req.params.userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        user.experiences.push({
+          role: req.body.role,
+          company: req.body.company,
+          startDate: req.body.startDate,
+          endDate: req.body.endDate,
+          area: req.body.area,
+   // add other fields as required
 });
-
-// Create an experience
-router.post('/api/users/:userId/experiences', (req, res) => {
-  db.Experience.create({
-    userId: req.params.userId,
-    title: req.body.title,
-    description: req.body.description,
-    // add other fields as required
-  })
-    .then((experience) => {
-      res.status(201).json(experience);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+user.save()
+.then((updatedUser) => {
+res.status(201).json(updatedUser.experiences[updatedUser.experiences.length - 1]);
+})
+.catch((err) => {
+res.status(500).send(err);
+});
+})
+.catch((err) => {
+res.status(500).send(err);
+});
 });
 
 // Get a specific experience
-router.get('/api/users/:userId/experiences/:expId', (req, res) => {
-  db.Experience.findOne({
-    where: {
-      userId: req.params.userId,
-      id: req.params.expId
+experiencesRouter.get('/api/users/:userId/experiences/:expId', (req, res) => {
+    User.findById(req.params.userId)
+    .then((user) => {
+    if (!user) {
+    return res.status(404).json({ message: 'User not found' });
     }
-  })
-    .then((experience) => {
-      if (!experience) {
-        return res.status(404).json({ message: 'Experience not found' });
-      }
-      res.json(experience);
+    const experience = user.experiences.id(req.params.expId);
+    if (!experience) {
+    return res.status(404).json({ message: 'Experience not found' });
+    }
+    res.json(experience);
     })
     .catch((err) => {
-      res.status(500).send(err);
+    res.status(500).send(err);
     });
-});
-
-// Edit a specific experience
-router.put('/api/users/:userId/experiences/:expId', (req, res) => {
-  db.Experience.findOne({
-    where: {
-      userId: req.params.userId,
-      id: req.params.expId
+    });
+    
+    // Edit a specific experience
+    experiencesRouter.put('/api/users/:userId/experiences/:expId', (req, res) => {
+    User.findById(req.params.userId)
+    .then((user) => {
+    if (!user) {
+    return res.status(404).json({ message: 'User not found' });
     }
-  })
-    .then((experience) => {
-      if (!experience) {
-        return res.status(404).json({ message: 'Experience not found' });
-      }
-      experience.title = req.body.title;
-     
-      experience.description = req.body.description;
-      // add other fields as required
-      experience.save()
-      .then((updatedExperience) => {
-      res.json(updatedExperience);
-      })
-      .catch((err) => {
-      res.status(500).send(err);
-      });
-      })
-      .catch((err) => {
-      res.status(500).send(err);
-      });
-      });
+    const experience = user.experiences.id(req.params.expId);
+    if (!experience) {
+    return res.status(404).json({ message: 'Experience not found' });
+    }
+    experience.role = req.body.role;
+    experience.company = req.body.company;
+    experience.startDate = req.body.startDate;
+    experience.endDate = req.body.endDate;
+    // add other fields as required
+    user.save()
+    .then((updatedUser) => {
+    res.json(updatedUser.experiences.id(req.params.expId));
+    })
+    .catch((err) => {
+    res.status(500).send(err);
+    });
+    })
+    .catch((err) => {
+    res.status(500).send(err);
+    });
+    });
+    
+    // Delete a specific experience
+    experiencesRouter.delete('/api/users/:userId/experiences/:expId', (req, res) => {
+    User.findById(req.params.userId)
+    .then((user) => {
+    if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+    }
+    const experience = user.experiences.id(req.params.expId);
+    if (!experience) {
+    return res.status(404).json({ message: 'Experience not found' });
+    }
+    experience.remove();
+    user.save()
+    .then(() => {
+    res.status(204).end();
+    })
+    .catch((err) => {
+        res.status(500).send(err);
+        });
+        })
+        .catch((err) => {
+        res.status(500).send(err);
+        });
+        });
       
-      // Delete a specific experience
-      router.delete('/api/users/:userId/experiences/:expId', (req, res) => {
-      db.Experience.destroy({
-      where: {
-      userId: req.params.userId,
-      id: req.params.expId
-      }
-      })
-      .then(() => {
-      res.status(204).end();
-      })
-      .catch((err) => {
-      res.status(500).send(err);
-      });
-      });
-      
-      // Change the experience picture
-      router.post('/api/profile/:userName/experiences/:expId/picture', upload.single('experiencePicture'), (req, res) => {
-      db.Experience.findOne({
-      where: {
-      userName: req.params.userName,
-      id: req.params.expId
-      }
-      })
-      .then((experience) => {
-      if (!experience) {
-      return res.status(404).json({ message: 'Experience not found' });
-      }
-      experience.picture = req.file.path;
-      experience.save()
-      .then((updatedExperience) => {
-      res.json(updatedExperience);
-      })
-      .catch((err) => {
-      res.status(500).send(err);
-      });
-      })
-      .catch((err) => {
-      res.status(500).send(err);
-      });
-      });
+    // Change the experience picture
+experiencesRouter.post('/api/profile/:userName/experiences/:expId/picture', upload.single('experiencePicture'), (req, res) => {
+    User.findOne({ userName: req.params.userName })
+    .then((user) => {
+    if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+    }
+    const experience = user.experiences.id(req.params.expId);
+    if (!experience) {
+    return res.status(404).json({ message: 'Experience not found' });
+    }
+    experience.image = req.file.path;
+    user.save()
+    .then(() => {
+    res.status(204).end();
+    })
+    .catch((err) => {
+    res.status(500).send(err);
+    });
+    })
+    .catch((err) => {
+    res.status(500).send(err);
+    });
+    });
       
       // Download the experiences as a CSV
-      router.get('/api/profile/:userName/experiences/CSV', (req, res) => {
-        db.Experience.findAll({
-          where: {
-            userName: req.params.userName
-          }
-        })
-          .then((experiences) => {
-            if (!experiences) {
-              return res.status(404).json({ message: 'No experiences found for this user' });
-            }
-            try {
-              const csv = json2csv(experiences);
-              res.attachment(`experiences-${req.params.userName}.csv`);
-              return res.status(200).send(csv);
-            } catch (err) {
-              return res.status(500).json({ message: 'Error generating CSV' });
-            }
-          })
-          .catch((err) => {
-            res.status(500).send(err);
-          });
-      });
+experiencesRouter.get('/api/profile/:userName/experiences/CSV', (req, res) => {
+    User.findOne({ userName: req.params.userName })
+    .then((user) => {
+    if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+    }
+    try {
+    const fields = ['role', 'company', 'startDate', 'endDate', 'description', 'area', 'image'];
+    const opts = { fields };
+    const csv = json2csv.parse(user.experiences, opts);
+    res.attachment('experiences-${req.params.userName}.csv');
+    return res.status(200).send(csv);
+    } catch (err) {
+    return res.status(500).json({ message: 'Error generating CSV' });
+    }
+    })
+    .catch((err) => {
+    res.status(500).send(err);
+    });
+    });
+    
+    
+    
 
       export default router
