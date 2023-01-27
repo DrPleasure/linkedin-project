4ef492291cd75d7e89b0f43adf1334b2bec0e63d
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2023-01-27 13:19:50 +0100

    .

--- front/src/components/feed/FeedPostLike.jsx
+++ front/src/components/feed/FeedPostLike.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { BsHandThumbsUp } from "react-icons/bs"
 import { FaRegCommentDots } from "react-icons/fa"
 import { BiRepost } from "react-icons/bi"
 import { RiSendPlaneFill } from "react-icons/ri"
 
-export default function FeedPostLike(props) {
+export default function FeedPostLike({ props }) {
+  const [showComments, setShowComments] = useState(false);
+
   return (
     <>
       <div
@@ -14,9 +16,9 @@ export default function FeedPostLike(props) {
         <div className="start-a-post-icon-text gray-hover">
           <BsHandThumbsUp style={{ fontSize: "20px" }} />
 
-          <span>{props.likes.length} Likes</span>
+          <span>{props.likes.length} Like</span>
         </div>
-        <div className="start-a-post-icon-text gray-hover ">
+        <div className="start-a-post-icon-text gray-hover " onClick={() => setShowComments(!showComments)}>
           <FaRegCommentDots style={{ fontSize: "20px" }} />
           <span>{props.comments.length} Comment</span>
         </div>
@@ -29,6 +31,11 @@ export default function FeedPostLike(props) {
           <span>Send</span>
         </div>
       </div>
+      {showComments && 
+        <div>
+          {props.map((comment, index) => <div key={index}>{comment.text}</div>)}
+        </div>
+      }
     </>
   )
 }
